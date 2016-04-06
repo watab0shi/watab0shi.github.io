b72318d287d2ec94012c3dee7c33e3d0e6b571ae
Wataru Kani <wat@wat-no-MacBook-Pro.local>
2016-04-06 22:40:28 +0900

    add 03_03

---
+++ 2016_04_3Days_PHP/03_03/mb_send_mail.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Session</title>
+</head>
+
+<body>
+	<?php
+		mb_language( "Japanese" );
+		mb_internal_encoding( "UTF-8" );
+
+		$to      = 'wtbs.kani@gmail.com';
+		$subject = '3DaysMaster_PHP';
+		$body    = 'This is a test mail.';
+
+		$result  = mb_send_mail( $to, $subject, $body );
+
+		if( $result ) echo 'Succeeded!';
+		else          echo 'Faild';
+	?>
+</body>
+
+</html>
