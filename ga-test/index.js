852145945f4e7938b0ec650506b6589d540d619c
wataru-kani <wataru-kani@aquaring.co.jp>
2021-10-01 20:41:16 +0900

    ga test js

--- ga-test/index.js
+++ ga-test/index.js
@@ -2,8 +2,10 @@ const onHashChange = () => {
   const hashname = location.hash.length > 0 ? location.hash.slice(1) : 'index';
   console.log('onHashChange', hashname);
 
-  ga('set', 'page', `/ga-test/${ hashname }.html`);
-  ga('send', 'pageview');
+  if (typeof window.ga === 'function') {
+    ga('set', 'page', `/ga-test/${ hashname }.html`);
+    ga('send', 'pageview');
+  }
 };
 
 const init = () => {
