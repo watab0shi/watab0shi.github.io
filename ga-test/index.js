6a4fd57e7569b8daca16c1f7c1eaa0b74db2baf1
wataru-kani <wataru-kani@aquaring.co.jp>
2021-10-01 21:54:49 +0900

    ga test js window -> let

--- ga-test/index.js
+++ ga-test/index.js
@@ -1,13 +1,13 @@
-window.isGACreated = false;
+let isGACreated = false;
 
 const onHashChange = () => {
   const hashname = location.hash.length > 0 ? location.hash.slice(1) : 'index';
   console.log('onHashChange', hashname);
 
   if (typeof window.ga === 'function') {
-    if (!window.isGACreated) {// 初回のみ create する
+    if (!isGACreated) {// 初回のみ create する
       ga('create', 'UA-209086237-1', 'auto');
-      window.isGACreated = true;
+      isGACreated = true;
     }
     ga('send', 'pageview', { 'page': `/ga-test/${ hashname }.html`, 'title': hashname });
   }
